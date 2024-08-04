b4423c80a8b7f77a0ed1761bbc1052c5a209ff43
lelek337 <ioan.af@mail.ru>
2024-08-04 17:31:25 +0300

    ддтотAdded classesfor serializer rereshtoken and assestoken

---
+++ src/main/java/com/example/securityjwt/AccessTokenJwsStringSerializer.java
@@ -0,0 +1,51 @@
+package com.example.securityjwt;
+
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.JWSSigner;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+import org.slf4j.LoggerFactory;
+
+import java.util.Date;
+import java.util.function.Function;
+import java.util.logging.Logger;
+
+public class AccessTokenJwsStringSerializer implements Function<Token, String> {
+
+    private final JWSSigner jwsSigner;
+    private JWSAlgorithm jwsAlgorithm;
+
+    public AccessTokenJwsStringSerializer(JWSSigner jwsSigner, JWSAlgorithm jwsAlgorithm) {
+        this.jwsSigner = jwsSigner;
+        this.jwsAlgorithm = jwsAlgorithm;
+    }
+
+    @Override
+    public String apply(Token token) {
+        var jwsHeader = new JWSHeader.Builder(this.jwsAlgorithm)
+                .keyID(token.id().toString())
+                .build();
+        var claimsSet = new JWTClaimsSet.Builder()
+                .jwtID(token.id().toString())
+                .subject(token.subject())
+                .issueTime(Date.from(token.expiresAt()))
+                .claim("authorities", token.authorities())
+                .build();
+        var signedJWT = new SignedJWT(jwsHeader, claimsSet);
+        try {
+            signedJWT.sign(this.jwsSigner);
+
+            return signedJWT.serialize();
+        } catch (JOSEException exception) {
+            LoggerFactory.getLogger(AccessTokenJwsStringSerializer.class)
+                    .error(exception.getMessage(), exception);
+        }
+        return null;
+    }
+
+    public void setJwsAlgorithm(JWSAlgorithm jwsAlgorithm) {
+        this.jwsAlgorithm = jwsAlgorithm;
+    }
+}
